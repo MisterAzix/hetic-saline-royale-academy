353e130e131fd38b8b8da24d0c8308365667db04
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2023-05-09 16:10:42 +0200

    chore: init storybook project ✨ (#3)

---
+++ libs/kit-ui/src/lib/kit-ui.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { KitUi } from './kit-ui';
+
+const Story: Meta<typeof KitUi> = {
+  component: KitUi,
+  title: 'KitUi',
+};
+export default Story;
+
+export const Primary = {
+  args: {},
+};
