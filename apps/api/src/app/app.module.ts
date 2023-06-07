257d410d74eaabda97b8e1d37e42846cbdde7ceb
Benoît Favrie <benoit14.favrie@gmail.com>
2023-06-07 13:32:53 +0200

    feat(api): update app.module and dependencies

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    //Config passeport to secure end-points
+    PassportModule,
+    //Config  env varibles
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    //Authentification password-jwt
+    AuthModule,
+    UsersModule,
+    // Config Pisma Client globally
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
