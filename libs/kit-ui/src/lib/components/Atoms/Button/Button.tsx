1ea16f3c72208cd0ecb38e83dac4e6e6af60bba4
MisterAzix <flizi@live.fr>
2023-06-07 12:05:01 +0200

    feat(kit-ui): improve Button component ✨

--- libs/kit-ui/src/lib/components/Atoms/Button/Button.tsx
+++ libs/kit-ui/src/lib/components/Atoms/Button/Button.tsx
@@ -1,62 +1,97 @@
 import { Button, buttonClasses } from '@mui/base';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { palette } from '../../../palette';
 import { typography } from '../../../typography';
 
 type ButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+type ButtonColor = 'primary' | 'secondary';
 
-const _Button = styled(Button)<{ size?: ButtonSize }>`
-  background-color: ${palette.primary[600]};
-  border-radius: 8px;
-  color: ${palette.white};
-  transition: all 150ms ease;
-  cursor: pointer;
-  border: none;
-  box-shadow: 0 4px 30px ${palette.gray[100]};
-
+const _Button = styled(Button)<{ size?: ButtonSize; color?: ButtonColor }>`
   ${({ size }) => {
     switch (size) {
       case 'xs':
-        return `
+        return css`
           ${typography.xs.semiBold};
           padding: 8px 14px;
         `;
       case 'sm':
-        return `
+        return css`
           ${typography.sm.semiBold};
           padding: 10px 16px;
         `;
       case 'md':
-        return `
+        return css`
           ${typography.md.semiBold};
           padding: 10px 18px;
         `;
       case 'lg':
-        return `
+        return css`
           ${typography.lg.semiBold};
           padding: 12px 20px;
         `;
       case 'xl':
-        return `
+        return css`
           ${typography.xl.semiBold};
           padding: 16px 28px;
         `;
       default:
-        return `
+        return css`
           ${typography.sm.semiBold};
           padding: 10px 16px;
         `;
     }
   }}
 
-  &:hover {
-    background-color: ${palette.primary[700]};
-  }
+  ${({ color }) => {
+    switch (color) {
+      case 'primary':
+      default:
+        return css`
+          background-color: ${palette.primary[600]};
+          border: 1px solid ${palette.primary[600]};
+          color: ${palette.white};
+
+          &:hover {
+            background-color: ${palette.primary[700]};
+            border: 1px solid ${palette.primary[700]};
+          }
+
+          &.${buttonClasses.active}, &.${buttonClasses.focusVisible} {
+            background-color: ${palette.primary[600]};
+            border: 1px solid ${palette.primary[600]};
+            box-shadow: 0 1px 2px rgba(16, 24, 40, 0.05),
+              0 0 0 4px ${palette.primary[100]};
+            outline: none;
+          }
+        `;
+      case 'secondary':
+        return css`
+          background-color: ${palette.white};
+          border: 1px solid ${palette.gray[300]};
+          color: ${palette.gray[700]};
+
+          &:hover {
+            background-color: ${palette.gray[50]};
+            color: ${palette.gray[800]};
+          }
+
+          &.${buttonClasses.active}, &.${buttonClasses.focusVisible} {
+            background-color: ${palette.white};
+            box-shadow: 0 1px 2px rgba(16, 24, 40, 0.05),
+              0 0 0 4px ${palette.gray[100]};
+            color: ${palette.gray[700]};
+            outline: none;
+          }
+        `;
+    }
+  }}
+
+  box-shadow: 0 1px 2px rgba(16, 24, 40, 0.05);
+  border-radius: 8px;
+  transition: all 150ms ease;
+  cursor: pointer;
 
   &.${buttonClasses.active}, &.${buttonClasses.focusVisible} {
-    background-color: ${palette.primary[600]};
-    box-shadow: 0 3px 20px 0 rgba(61, 71, 82, 0.1),
-      0 0 0 5px ${palette.primary[100]};
     outline: none;
   }
 
