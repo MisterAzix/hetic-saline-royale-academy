ef5c426c8930d4e65db3506eea6b5b5510b38572
Julian Laballe <julian.laballe@unlikely.technology>
2023-06-08 12:55:09 +0200

    feat(kit-ui): create Abstracts component Grid

---
+++ libs/kit-ui/src/lib/components/Abstracts/Grid/Grid.tsx
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components';
+import { from, getColumnConfig, getTheme } from '../../../../../styles/mixins';
+import { MAX } from '../../../../../styles/theme';
+import { PropsWithChildren } from 'react';
+
+const GridComponent = styled.section<{ isLarge?: boolean }>`
+  display: grid;
+  width: 100vw;
+  max-width: 100%;
+  margin-inline: auto;
+  ${getColumnConfig('mobile')};
+  ${from('md', getColumnConfig('tablet'))}
+  ${from('lg', getColumnConfig('desktop'))}
+  ${from(
+    'maxGridWidth',
+    css`
+      width: ${getTheme(MAX, 'width')}px;
+    `
+  )}
+`;
+
+type GridProps = PropsWithChildren;
+
+function Grid({ children }: GridProps) {
+  return <GridComponent>{children}</GridComponent>;
+}
+
+export default Grid;
