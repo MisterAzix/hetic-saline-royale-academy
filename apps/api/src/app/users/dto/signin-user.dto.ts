1062660f3d362370a33e0ff7494c51c9ce1386e8
Justin katasi <108656766+justinDev91@users.noreply.github.com>
2023-06-09 14:13:03 +0200

    feat(api): config swagger for api documentation (#29)

--- apps/api/src/app/users/dto/signin-user.dto.ts
+++ apps/api/src/app/users/dto/signin-user.dto.ts
@@ -1,11 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
-
 // Class validator permet de valider les données entrantes des endpoints
 export class UserSignDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,14 +5,19 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
+import { config } from './swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
