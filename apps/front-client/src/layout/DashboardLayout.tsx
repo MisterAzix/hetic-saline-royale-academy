27471abf767bc0a9179cc4d6e1ac8871ce5f534e
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2023-07-04 11:46:13 +0200

    feat(front-client): components refacto

--- apps/front-client/src/layout/DashboardLayout.tsx
+++ apps/front-client/src/layout/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import Header from '../features/header/components/Organisms/Header';
 import Sidebar from '../features/sidebar/components/Organisms/Sidebar';
 
@@ -9,12 +9,12 @@ const DashboardLayout = () => {
       gridTemplateColumns="250px 1fr"
       sx={{ height: '100vh' }}
     >
-      <Box>
+      <Stack>
         <Sidebar />
-      </Box>
-      <Box>
+      </Stack>
+      <Stack>
         <Header />
-      </Box>
+      </Stack>
     </Box>
   );
 };
