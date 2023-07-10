9108c7d5d192baa8ce5380492a3806b3c5e34af2
Justin katasi <108656766+justinDev91@users.noreply.github.com>
2023-07-10 15:33:17 +0200

    feat(api): add  nestjs memory cache globaly (#48)

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,13 +1,16 @@
+import {
+  CacheInterceptor,
+  CacheModule,
+  CacheModuleOptions,
+} from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
-
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { PassportModule } from '@nestjs/passport';
 import { AchievementModule } from './achievement/achievement.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma.module';
-import { UsersModule } from './users/users.module';
 import { BadgeModule } from './badge/badge.module';
 import { CategoryModule } from './category/category.module';
 import { ChapterModule } from './chapter/chapter.module';
@@ -16,15 +19,22 @@ import { GamificationModule } from './gamification/gamification.module';
 import { ImageModule } from './image/image.module';
 import { LessonModule } from './lesson/lesson.module';
 import { NotificationModule } from './notification/notification.module';
+import { PrismaModule } from './prisma.module';
 import { ProgressTrackerModule } from './progress_tracker/progress_tracker.module';
 import { RessourceModule } from './ressource/ressource.module';
 import { RewardsModule } from './rewards/rewards.module';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { TagModule } from './tag/tag.module';
+import { UsersModule } from './users/users.module';
 import { VideoModule } from './video/video.module';
 
 @Module({
   imports: [
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 40,
+      max: 100,
+    } as CacheModuleOptions),
     //Config passeport to secure end-points
     PassportModule,
     //Config  env varibles
@@ -54,6 +64,12 @@ import { VideoModule } from './video/video.module';
     VideoModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
