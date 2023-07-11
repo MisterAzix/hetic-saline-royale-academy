de94bf99a640095997d6002c32e12c45328958a9
Julian Laballe <julian.laballe@unlikely.technology>
2023-07-11 11:57:52 +0200

    feat(kit-ui): rename mui.ts to mui.d.ts

--- libs/kit-ui/styles/@types/mui.d.ts
+++ libs/kit-ui/styles/@types/mui.d.ts
@@ -11,6 +11,9 @@ import type {PaletteOptions, Palette} from '@mui/material'
  * @description
  * We need to extend the MUI theme types to add our custom typography and colors.
  * Otherwise, we'll get a type error when trying to use them.
+ *
+ * This file is present in front-admin, front-client and kit-ui.
+ * NX will throw a type error when trying to push if we don't put it in all workspaces.
  */
 declare module '@mui/material/styles' {
   //eslint-disable-next-line @typescript-eslint/no-empty-interface
