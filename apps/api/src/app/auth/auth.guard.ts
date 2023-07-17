eec5098227b00c5db449e5676f6727832e1de942
Justin katasi <108656766+justinDev91@users.noreply.github.com>
2023-07-17 10:34:10 +0200

    Update auth.guard.ts

--- apps/api/src/app/auth/auth.guard.ts
+++ apps/api/src/app/auth/auth.guard.ts
@@ -28,7 +28,7 @@ export class AuthGuard implements CanActivate {
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException(
-         `Failed to extract token. Invalid token or incorrect email/password.`
+         `Failed to extract token. Invalid token.`
       );
     }
     try {
