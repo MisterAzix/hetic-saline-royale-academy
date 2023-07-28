ea5c8032920699c29e66eb8b56967a0350bb9c4b
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2023-07-28 18:06:04 +0200

    fix(api): enable routes validation 🩹

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,26 +1,18 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 import { AuthorInterceptor } from './interceptors/author.interceptor';
 import { config } from './swagger.config';
 
+const GLOBAL_PREFIX = 'api';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+
+  app.setGlobalPrefix(GLOBAL_PREFIX);
   app.useGlobalInterceptors(new AuthorInterceptor());
-
-  const port = process.env.PORT || 3000;
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: [
       process.env.CLIENT_URL || 'http://localhost:8080',
@@ -31,9 +23,14 @@ async function bootstrap() {
     credentials: true,
   });
 
+  const port = process.env.PORT || 3000;
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`
   );
 }
 
