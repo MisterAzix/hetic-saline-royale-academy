d35e8a49d8d23204465f03a806c67b87c477b48a
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2023-08-12 19:36:38 +0200

    fix(api): clean useless files 🔥 (#70)

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -8,8 +8,6 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { PassportModule } from '@nestjs/passport';
 import { AchievementModule } from './achievement/achievement.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { BadgeModule } from './badge/badge.module';
 import { CategoryModule } from './category/category.module';
@@ -63,9 +61,7 @@ import { VideoModule } from './video/video.module';
     TagModule,
     VideoModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_INTERCEPTOR,
       useClass: CacheInterceptor,
