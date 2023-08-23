8e0970c06f6feb35d5808145adefd8f4ab7467a4
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2023-08-23 17:13:01 +0200

    feat(api): do not use AuthorInterceptor globally 🩹 (#83)

--- apps/api/src/app/achievement/achievement.module.ts
+++ apps/api/src/app/achievement/achievement.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AchievementService } from './achievement.service';
 import { AchievementController } from './achievement.controller';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AuthorInterceptor } from '../../interceptors';
 
 @Module({
   controllers: [AchievementController],
-  providers: [AchievementService]
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AuthorInterceptor,
+    },
+    AchievementService,
+  ],
 })
 export class AchievementModule {}
--- apps/api/src/app/badge/badge.module.ts
+++ apps/api/src/app/badge/badge.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { BadgeService } from './badge.service';
 import { BadgeController } from './badge.controller';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AuthorInterceptor } from '../../interceptors';
 
 @Module({
   controllers: [BadgeController],
-  providers: [BadgeService]
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AuthorInterceptor,
+    },
+    BadgeService,
+  ],
 })
 export class BadgeModule {}
