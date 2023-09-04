c638b939a9899c31525cc18999f316be37335540
justinDev91 <justin@sustainsoft.eu>
2023-09-04 13:38:46 +0200

    feat(front-client): implement logout

--- apps/front-client/src/features/sidebar/components/Molecules/Profile.tsx
+++ apps/front-client/src/features/sidebar/components/Molecules/Profile.tsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
 import { palette } from '@hetic-saline-royale-academy/kit-ui';
+import { Person } from '@mui/icons-material';
 import { Box, Stack, SvgIcon } from '@mui/material';
-import TitleText from '../Atoms/TitleText';
-import DescriptionText from '../Atoms/DescriptionText';
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { routes } from '../../../../routes';
-import { useSession } from 'next-auth/react';
-import { Person } from '@mui/icons-material';
+import DescriptionText from '../Atoms/DescriptionText';
+import TitleText from '../Atoms/TitleText';
 
 const ProfileContainer = styled(Stack)`
   flex-direction: row;
@@ -30,6 +30,10 @@ const Profile = () => {
   const session = useSession();
   const user = session?.data?.user;
 
+  const handleLogout = async () => {
+    await signOut({ callbackUrl: '/' });
+  };
+
   if (!user) {
     return null;
   }
@@ -38,29 +42,34 @@ const Profile = () => {
   const email = user.email || '';
 
   return (
-    <Link href={routes.profile}>
-      <ProfileContainer>
-        <Box
-          sx={{
-            borderRadius: '50%',
-            backgroundColor: palette.gray[100],
-            width: '40px',
-            height: '40px',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <SvgIcon sx={{ color: palette.gray[900] }}>
-            <Person />
-          </SvgIcon>
-        </Box>
-        <ProfileDescription>
-          <TitleText>{fullName}</TitleText>
-          <DescriptionText>{email}</DescriptionText>
-        </ProfileDescription>
-      </ProfileContainer>
-    </Link>
+    <>
+      <Link href={routes.profile}>
+        <ProfileContainer>
+          <Box
+            sx={{
+              borderRadius: '50%',
+              backgroundColor: palette.gray[100],
+              width: '40px',
+              height: '40px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <SvgIcon sx={{ color: palette.gray[900] }}>
+              <Person />
+            </SvgIcon>
+          </Box>
+          <ProfileDescription>
+            <TitleText>{fullName}</TitleText>
+            <DescriptionText>{email}</DescriptionText>
+          </ProfileDescription>
+        </ProfileContainer>
+      </Link>
+      <TitleText>
+        <button onClick={handleLogout}>Logout</button>
+      </TitleText>
+    </>
   );
 };
 
