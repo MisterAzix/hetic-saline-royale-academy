59761f26781cedb6703883e6d2b56e1aaf9d2486
justinDev91 <justin@sustainsoft.eu>
2023-09-05 18:56:22 +0200

    feat(front-client): display user's subscribed courses

--- apps/api/src/app/course/course.module.ts
+++ apps/api/src/app/course/course.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthorInterceptorModule } from '../../interceptors/author.interceptor.module';
 import { CourseController } from './course.controller';
 import { CourseService } from './course.service';
-
 @Module({
   controllers: [CourseController],
   providers: [CourseService],
--- apps/api/src/app/rewards/rewards.module.ts
+++ apps/api/src/app/rewards/rewards.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthorInterceptorModule } from '../../interceptors/author.interceptor.module';
 import { RewardsController } from './rewards.controller';
 import { RewardsService } from './rewards.service';
-
 @Module({
   controllers: [RewardsController],
   providers: [RewardsService],
