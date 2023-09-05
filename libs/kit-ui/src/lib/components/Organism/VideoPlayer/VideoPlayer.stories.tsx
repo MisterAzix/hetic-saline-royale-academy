a51844a475c45430f23ef0ab95618a7848192fdd
Julian Laballe <julian.laballe@unlikely.technology>
2023-09-05 20:39:36 +0200

    feat: add VideoPlayer component

---
+++ libs/kit-ui/src/lib/components/Organism/VideoPlayer/VideoPlayer.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import VideoPlayer from './VideoPlayer';
+import { StorybookTitle } from '../../../../../@types';
+
+const meta: Meta<typeof VideoPlayer> & {
+  title: StorybookTitle<'VideoPlayer'>;
+} = {
+  component: VideoPlayer,
+  tags: ['autodocs'],
+  title: 'Organism/VideoPlayer',
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+export default meta;
+type Story = StoryObj<typeof VideoPlayer>;
+
+export const Playground: Story = {
+  args: {},
+};
