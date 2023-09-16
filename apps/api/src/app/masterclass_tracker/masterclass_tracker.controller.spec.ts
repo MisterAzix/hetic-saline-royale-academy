680962ae768d5cc0c50347351769fe95041eaa72
balayette_ <louis.fortrie4@gmail.com>
2023-09-16 15:12:43 +0200

    feat(api): fix providers in test

--- apps/api/src/app/masterclass_tracker/masterclass_tracker.controller.spec.ts
+++ apps/api/src/app/masterclass_tracker/masterclass_tracker.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MasterclassTrackerController } from './masterclass_tracker.controller';
 import { MasterclassTrackerService } from './masterclass_tracker.service';
+import { PrismaService } from '../../prisma.service';
 
 describe('MasterclassTrackerController', () => {
   let controller: MasterclassTrackerController;
@@ -8,7 +9,7 @@ describe('MasterclassTrackerController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MasterclassTrackerController],
-      providers: [MasterclassTrackerService],
+      providers: [MasterclassTrackerService, PrismaService],
     }).compile();
 
     controller = module.get<MasterclassTrackerController>(
--- apps/api/src/app/masterclass_tracker/masterclass_tracker.service.spec.ts
+++ apps/api/src/app/masterclass_tracker/masterclass_tracker.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MasterclassTrackerService } from './masterclass_tracker.service';
+import { PrismaService } from '../../prisma.service';
 
 describe('MasterclassTrackerService', () => {
   let service: MasterclassTrackerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MasterclassTrackerService],
+      providers: [MasterclassTrackerService, PrismaService],
     }).compile();
 
     service = module.get<MasterclassTrackerService>(MasterclassTrackerService);
